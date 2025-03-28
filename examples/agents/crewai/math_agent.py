00cbe58827be997c823cb967cf35cf7d1de7e293
Paolo Dettori <dettori@us.ibm.com>
2025-03-27 22:25:42 -0400

    add license headers
    Signed-off-by: Paolo Dettori <dettori@us.ibm.com>

--- examples/agents/crewai/math_agent.py
+++ examples/agents/crewai/math_agent.py
@@ -1,3 +1,17 @@
+# Copyright 2025 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import crew
 from langchain_community.tools import tool
@@ -85,10 +99,3 @@ class MathAgent:
 
     def getCrew(self) -> Crew:
         return self.crew    
-
-# # run the crew
-# if __name__ == "__main__":
-#     prompt = "add 3 + 5 and divide the result by 2"
-#     instance = MathAgent("ollama/llama3.2:3b-instruct-fp16", "http://localhost:11434", step_callback).getCrew()
-#     result = instance.kickoff(inputs={'prompt': prompt})
-#     print(result)
