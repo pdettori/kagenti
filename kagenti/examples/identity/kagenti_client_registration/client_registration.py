babe75ae2c6d9047d558e3de42a5244fd2bfe078
Alan Cha <Alan.cha1@ibm.com>
2025-07-24 14:28:16 -0400

    Add NAMESPACE to client registration
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- kagenti/examples/identity/kagenti_client_registration/client_registration.py
+++ kagenti/examples/identity/kagenti_client_registration/client_registration.py
@@ -8,6 +8,7 @@ KEYCLOAK_REALM = os.environ.get('KEYCLOAK_REALM')
 KEYCLOAK_ADMIN_USERNAME = os.environ.get('KEYCLOAK_ADMIN_USERNAME')
 KEYCLOAK_ADMIN_PASSWORD = os.environ.get('KEYCLOAK_ADMIN_PASSWORD')
 CLIENT_NAME = os.environ.get('CLIENT_NAME')
+NAMESPACE = os.environ.get('NAMESPACE')
 
 if KEYCLOAK_URL is None:
     raise Exception('Expected environment variable "KEYCLOAK_URL"')
@@ -19,24 +20,32 @@ if KEYCLOAK_ADMIN_PASSWORD is None:
     raise Exception('Expected environment variable "KEYCLOAK_ADMIN_PASSWORD"')
 if CLIENT_NAME is None:
     raise Exception('Expected environment variable "CLIENT_NAME"')
+if NAMESPACE is None:
+    raise Exception('Expected environment variable "NAMESPACE"')
 
 keycloak_admin = KeycloakAdmin(
-            server_url=KEYCLOAK_URL,
-            username=KEYCLOAK_ADMIN_USERNAME,
-            password=KEYCLOAK_ADMIN_PASSWORD,
-            realm_name=KEYCLOAK_REALM,
-            user_realm_name='master')
+    server_url=KEYCLOAK_URL,
+    username=KEYCLOAK_ADMIN_USERNAME,
+    password=KEYCLOAK_ADMIN_PASSWORD,
+    realm_name=KEYCLOAK_REALM,
+    user_realm_name='master'
+)
+
+clientId = f'{NAMESPACE}/{CLIENT_NAME}'
 
 # Create client
 try:
-    llama_stack_client_id = keycloak_admin.create_client({
-        "clientId": CLIENT_NAME,
-        "standardFlowEnabled": True,
-        "directAccessGrantsEnabled": True,
-        "fullScopeAllowed": False,
-        "publicClient": True # Disable client authentication
-    })
+    llama_stack_client_id = keycloak_admin.create_client(
+        {
+            "name": CLIENT_NAME,
+            "clientId": clientId,
+            "standardFlowEnabled": True,
+            "directAccessGrantsEnabled": True,
+            "fullScopeAllowed": False,
+            "publicClient": True # Disable client authentication
+        }
+    )
 
-    logging.info("Created Keycloak client")
+    logging.info(f'Created Keycloak client "{clientId}"')
 except KeycloakPostError as e:
-    logging.error(f'Could not create Keycloak client "{CLIENT_NAME}": {e}')
+    logging.error(f'Could not create Keycloak client "{clientId}": {e}')
