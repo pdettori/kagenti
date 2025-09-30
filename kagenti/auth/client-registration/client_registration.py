fe36f4a2082990f9af886e3b5860d5cca3800a61
Mariusz Sabath <mrsabath@gmail.com>
2025-09-29 20:29:17 -0400

    Internal client id
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- kagenti/auth/client-registration/client_registration.py
+++ kagenti/auth/client-registration/client_registration.py
@@ -71,10 +71,10 @@ def register_client(
         client_ids = [client['clientId'] for client in clients]
         print(client_ids)
         
-        cl_id = keycloak_admin.get_client_id("{client_name}")
-        print(f'Retrieving client_id for client "{client_name}", id: {cl_id}.')
-        secret = keycloak_admin.get_client_secrets(cl_id)["value"]
-        print(f'Successfully retrieved secret for client "{client_name}".')
+        internal_client_id = keycloak_admin.get_client_id("{client_name}")
+        print(f'Retrieving client_id for client "{client_name}", id: {internal_client_id}.')
+        secret = keycloak_admin.get_client_secrets(internal_client_id)["value"]
+        print(f'Successfully retrieved secret for client "{client_name}": {secret}.')
     except KeycloakPostError as e:
         print(f"Could not retrieve secret for client '{client_name}': {e}")
         return
